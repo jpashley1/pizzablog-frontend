c6fd2e182acbf3b70c545ea4c06d2b331ae4c6ef
jpashley1 <jpashley1@gmail.com>
2024-10-22 22:25:11 -0400

    add recipes to page and redeploy

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+
 export function Home() {
+  const [recipes, setRecipes] = useState([]);
+
+  const handleIndex = () => {
+    console.log("handleIndex");
+    axios.get("https://pizzablog-api.onrender.com/recipes.json").then((response) => {
+      console.log(response.data);
+      setRecipes(response.data);
+    });
+  };
+
+  useEffect(handleIndex, []);
   return (
     <main>
-      <h1>PizzaDreams</h1>
+      <h1>PizzaDreams Recipes</h1>
     </main>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import axios from 'axios'
+axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://pizzablog-frontend.onrender.com/";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
